2281a47068ef13a3318dc0a8c4aa39ef4bc007e8
ranjeet <ranjeetvit2012@gmail.com>
2025-05-04 09:22:38 +0530

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import db from "@repo/db/client"
+const app = express()
+const port = 8080
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  db.user.findMany()
+    .then((users:any) => {
+      res.json(users);
+    })
+    .catch((err:any) => {
+      res.status(500).json({ error: err.message });
+    });
+})
+
+app.post("/user", (req, res) => {
+  const { username, password } = req.body;
+  
+  if (!username || !password) {
+    res.status(400).json({ error: "Username and password are required" });
+    return
+  }
+
+  db.user.create({
+    data: {
+      username,
+      password
+    }
+  })
+    .then((user:any) => {
+      res.status(201).json(user);
+    })
+    .catch((err:any) => {
+      res.status(500).json({ error: err.message });
+    });
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ apps/websocket/src/index.ts
@@ -0,0 +1,28 @@
+import db from "@repo/db/client";
+console.log("dd")
+
+Bun.serve({
+    port: 8081,
+  
+    fetch(req, server) {
+      if (server.upgrade(req)) return; // handle WebSocket upgrade
+      return new Response("Upgrade failed", { status: 500 });
+    },
+  
+    websocket: {
+      async message(ws, message) {
+        try {
+          await db.user.create({
+            data: {
+              username: Math.random().toString(36).substring(2),
+              password: Math.random().toString(36).substring(2),
+            },
+          });
+          ws.send(`Echo: ${message}`);
+        } catch (err) {
+          console.error("DB error:", err);
+          ws.send("Error occurred");
+        }
+      },
+    },
+  });
